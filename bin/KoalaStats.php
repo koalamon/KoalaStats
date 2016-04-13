125e34f52b0396e79dd363067110829c299c90f6
Nils Langner <contact@phmlabs.com>
2016-04-13 20:12:10 +0200

    initial commit

---
+++ bin/KoalaStats.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!$loader = include __DIR__ . '/../vendor/autoload.php') {
+    die('You must set up the project dependencies.');
+}
+
+$app = new \Cilex\Application('KoalaStats', '##development##');
+$app->command(new \Koalamon\KoalaStats\Command\RunCommand());
+$app->run();
---
+++ src/Command/RunCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Koalamon\KoalaStats\Command;
+
+use GuzzleHttp\Client;
+use Koalamon\Client\Reporter\Event;
+use Koalamon\Client\Reporter\Reporter;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RunCommand extends \Cilex\Command\Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('run')
+            ->setDescription('Run command and send result to koalamon')
+            ->addArgument('shellCommand', InputArgument::REQUIRED, 'The command line command')
+            ->addArgument('eventIdentifier', InputArgument::REQUIRED, 'The event identifer')
+            ->addArgument('projectApiKey', InputArgument::REQUIRED, 'The project api key')
+            ->addArgument('systemIdentifier', InputArgument::REQUIRED, 'The system identifier')
+            ->addArgument('toolIdentifier', InputArgument::REQUIRED, 'The tool identifier')
+            ->addOption('koalamonServer', 's', InputOption::VALUE_OPTIONAL, 'The koalamon server', 'http://monitor.koalamon.com')
+            ->addOption('eventUrl', 'u', InputOption::VALUE_OPTIONAL, 'An url representing this event');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $command = $input->getArgument('shellCommand');
+        $identifier = $input->getArgument('eventIdentifier');
+        $projectApiKey = $input->getArgument('projectApiKey');
+        $systemIdentifier = $input->getArgument('systemIdentifier');
+        $toolIdentifier = $input->getArgument('toolIdentifier');
+        $url = $input->getOption('eventUrl');
+        $koalamonServer = $input->getOption('koalamonServer');
+
+        exec($command, $value, $returnCode);
+
+        if ($returnCode == 0) {
+            $reporter = new Reporter('', $projectApiKey, new Client(), $koalamonServer);
+            $event = new Event($identifier, $systemIdentifier, Event::STATUS_SUCCESS, $toolIdentifier, '', $value[0], $url);
+
+            $reporter->sendEvent($event);
+
+            $output->writeln("\n  <info>Stats successfully send.</info>\n");
+        }
+    }
+}
